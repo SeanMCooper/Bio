9a0606d6cf4722815aadc9bef1ab59016726f559
unknown <seancooper.exe@gmail.com>
2018-09-20 23:48:08 -0700

    pushing

---
+++ logic/buttons.js
@@ -0,0 +1,7 @@
+$("#submitContact").on("click", function(){
+    $("#newName").val("")
+    $("#newEmail").val("")
+    $("#newMessage").val("")
+    alert("Message delivered");
+    console.log("hi")
+});
