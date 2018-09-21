45f6bd0397da4a8fbc50017a8d5cb06226e68fa5
unknown <seancooper.exe@gmail.com>
2018-09-21 00:01:17 -0700

    bug

--- logic/buttons.js
+++ logic/buttons.js
@@ -1,7 +1,15 @@
 $("#submitContact").on("click", function(){
-    $("#newName").val("")
-    $("#newEmail").val("")
-    $("#newMessage").val("")
+
+        $("#newName").val(""),
+        $("#newEmail").val(""),
+        $("#newMessage").val("")
+    
     alert("Message delivered");
-    console.log("hi")
-});
+})
+
+$("#movieAppButton").on("click", function(){
+    location.replace("https://seanmcooper.github.io/moviePal/")
+})
+$("#caterAppButton").on("click", function(){
+    location.replace("https://cater-app.herokuapp.com/")
+})
